a0b14f31dc1f075ca066ca320cb893038dc94ea4
Noah Sweeting <nsweeting2@gmail.com>
2025-11-17 14:03:45 -0500

    .

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -117,13 +117,13 @@ if [ -f "$original_file" ]; then
     mv "$original_file" "${original_file}_${timestamp}.bak"
 fi
 
-find $original_file -type l -delete
+# find $original_file -type l -delete
 
 dank_file="$HOME/.dank-dots/.config/DankMaterialShell/settings.json"
-#if [ ! -f "$dank_file" ]; then
+if [ ! -f "$dank_file" ]; then
     # Create a symbolic link and force creation
     ln -snf $dank_file $original_file
-#fi
+fi
 
 # Backup Hypr .conf files, then symlink from .dank-dots
 # dank_file="$HOME/.dank-dots/.config/hypr/hyprland.conf"
